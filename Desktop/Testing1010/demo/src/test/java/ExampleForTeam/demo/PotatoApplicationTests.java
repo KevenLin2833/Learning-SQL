3827bf2369a42871d41e2b59c6945049b425e22b
keven <keven@iastate.edu>
2020-08-28 16:00:06 -0500

    testingstuff

---
+++ Desktop/Testing1010/demo/src/test/java/ExampleForTeam/demo/PotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package ExampleForTeam.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
